f88e95d0d370dc9631372a858d4d151fcca5a30f
Tin Nguyen <tinng95@yahoo.com>
2016-02-03 15:33:10 -0600

    Initial Commit

---
+++ C Programming PA1/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double attackCaculator(float critChance, double critDamage, double attack, float physicalResistance) 
+{
+	double random;
+	random = rand() % 100, 1;
+
+	if (random <= critChance) {
+		attack = (attack * ((critDamage/100)+1)) * (physicalResistance / 100);}
+		//attack is INT, CritDamage is Float, physicalResistance is DOUBLE
+	else {
+		attack = attack *(physicalResistance / 100);
+	}
+	return attack;
+};
+
+
+
+int main() {
+	char name[50];
+	int  ID;
+
+	int health = 500;
+	int attack= 40;
+	
+	double physicalResistance;
+	//physicalResistance = rand() % 100, 1;
+	double critChance;
+	//critChance = rand() % 100 + 1;
+
+	float critDamage;
+	//critDamage = rand() % 500 + 100;
+	
+	long lumion; // Currency;
+	double temp = (double)rand();
+
+	printf("%lf", temp);
+
+	return 0;
+};
