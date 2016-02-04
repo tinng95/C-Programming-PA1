4eea7e8cca5b7fe2a3c2b0598aec7ca2d6967786
Tin Nguyen <tinng95@yahoo.com>
2016-02-04 11:49:06 -0600

    Finished Attack Caculator and introduction

--- C Programming PA1/main.c
+++ C Programming PA1/main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-double attackCaculator(float critChance, double critDamage, double attack, float physicalResistance) 
+
+void attackCaculator(double playerCritChance, double playerCritDamage,  double playerPhysicalResistance, int attack, int HP)
 {
 	double random;
 	random = rand() % 100, 1;
+	playerCritDamage = rand() % 500 + 100;
+	playerCritChance = rand() % 100 + 1;
+	playerPhysicalResistance = rand() % 80 + 20;
 
-	if (random <= critChance) {
-		attack = (attack * ((critDamage/100)+1)) * (physicalResistance / 100);}
-		//attack is INT, CritDamage is Float, physicalResistance is DOUBLE
+	if (random <= playerCritChance) {
+		attack = (int)(((double)attack * (playerCritDamage /100)) * (playerPhysicalResistance / 100));}
 	else {
-		attack = attack *(physicalResistance / 100);
+		attack = (int)((double)attack *(playerPhysicalResistance / 100));
 	}
-	return attack;
-};*/
+
+	HP = HP - attack;
+};
 
 
 void createPlayerCharacter(char *roleName, int *playerHealth, int *playerAttack, double *playerResistance, double *playerCritChance, double *playerCritDamage)
@@ -24,9 +27,17 @@ void createPlayerCharacter(char *roleName, int *playerHealth, int *playerAttack,
 		case 1:
 			break;
 	}
-}
-
 };
+
+void introduction(int warriorHP, int mageHP, int archerHP, int warriorAttack, int mageAttack, int archerAttack) {
+	printf("Welcome to your Programming Assignment 1!/n");
+	printf("In this assigment, you will have to choose one of the 3 classesto defeat Lord Evil./n");
+	printf("You can choose to be a WARRIOR, a MAGE or an Archer varied by thier stats./n");
+    printf("1. Warrior's HP: %d , Warrior's Attack: %d/n", warriorHP,warriorAttack);
+	printf("2. Mage's HP: %d , Mage's Attack: %d/n", mageHP, mageAttack);
+	printf("3. Archer's HP: %d , Archer's Attack: %d/n", archerHP, archerAttack);
+	printf("Please select your class with one of the number above: ");
+}
 
 
 
@@ -43,12 +54,16 @@ int main() {
 	int mageHP;
 	int archerHP;
 
+	int warriorAttack;
+	int mageAttack;
+	int archerAttack;
+
 	//CHARACTER STATS
 	double playerPhysicalResistance;
 	double playerCritChance;
 	double playerCritDamage;
 	
-	
+	attackCaculator(playerCritChance, playerCritDamage, playerPhysicalResistance, warriorAttack, &warriorHP);
 	/*
 	char name[50];
 	int  ID;
