16099c72a4ffe0c8162dab60814f3dbd6f19bb80
Tin Nguyen <tinng95@yahoo.com>
2016-02-05 08:17:22 -0600

    made playerMove function, renamesome variables

--- C Programming PA1/main.c
+++ C Programming PA1/main.c
@@ -71,33 +71,41 @@ void createPlayerCharacter(int roleNumber, int *playerHealth, int *playerAttack,
 	}
 };
 
-void createBoss(int *bossHealth, int *bossAttack, double *bossPhysicalResistance, double *bossCritChance, double *bossCritDamage)
+void createBoss(int *bossHP, int *bossAttack, double *bossPhysicalResistance, double *bossCritChance, double *bossCritDamage)
 {
 	/*BEGINNING of your solution*/
-	*bossHealth = 50000; //Set *bossHealth to 50000
+	*bossHP = 50000; //Set *bossHealth to 50000
 	*bossAttack = (rand() % 600 + 400); //Set *bossAttack to a randomized number in the range between 400 to 600 inclusively
 	*bossPhysicalResistance = (rand() % 60 + 30) / 100; //Set *bossPhysicalResistance to a randomized number in the range between 30 to 60 inclusively
 	*bossCritChance = (rand() % 20 + 10) / 100; //Set *bossCritChance to a randomized number in the range between 10 to 20 inclusively
 	*bossCritDamage = (rand() % 250 + 100) / 100; //Set *bossCritDamage to a randomized number in the range between 100 to 250 inclusively
 	/*END of your solution*/
 };
+
 void introduction() {
 	printf("Welcome to your Programming Assignment 1!\n");
 	printf("In this assigment, you will have to choose one of the 3 classesto defeat Silva.\n");
-	printf("You can choose to be a WARRIOR, a MAGE or an Archer.\n ");
+	printf("You can choose to be a WARRIOR, a MAGE or an ARCHER.\n ");
 	printf("1. Warrior\n ");
 	printf("2. Mage\n ");
 	printf("3. Archer\n");
 	printf("Please select your class with the coresponding numbers: ");
 }
 
+int playerMove() {
+	int move;
+	printf("1. Attack\n");
+	printf("2. Heal\n");
+	scanf("%d", &move);
+	return move;
+}
+
+
 int getUserInput() {
 	int number;
 	scanf("%d", &number);
 	return number;
 }
-
-
 
 int main() {
 	//Student Info
@@ -116,18 +124,20 @@ int main() {
 	double playerCritChance;
 	double playerCritDamage;
 	
-	
 	//BOSS STATS
 	char bossName[10] = { 'S', 'i', 'l', 'v', 'a' };
 	int bossHP;
 	int bossAttack;
 	double bossPhysicalResistance;
-	double bosserCritChance;
-	double bosserCritDamage;
+	double bossCritChance;
+	double bossCritDamage;
 	
 	//FUNCTION
 	introduction();
 	createPlayerCharacter(getUserInput(), &playerHealth, &playerAttack, &playerPhysicalResistance, &playerCritChance, &playerCritDamage);
+	createBoss(&bossHP, &bossAttack, &bossPhysicalResistance, &bossCritChance, &bossCritDamage);
+
+
 
 	return 0;
 };
